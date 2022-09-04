d7614ded4edc7f6ece0de76069f7780dc7cb77cc
Emily Bartman <frank@emwaves.org>
2022-09-03 19:18:27 -0700

    add: tailwind and try pinging cowsay

--- netlify/functions/cowsay.ts
+++ netlify/functions/cowsay.ts
@@ -4,6 +4,9 @@ import cowsay from "cowsayjs";
 const handler: Handler = async (event, context) => {
   // your server-side functionality
   return {
+    headers: {
+      "Access-Control-Allow-Origin": "http://localhost:8080/", 
+    },
     statusCode: 200,
     body: JSON.stringify({
       message: cowsay.moo("moooo00OOOOOOOOooooo..."),
